03b1291728d9b2b29f161c37b2e68377c81d941a
mainak-wq <mainakdeb1998@gmail.com>
2024-01-05 17:19:08 +0530

    MSIT BCA FIRST DAY

---
+++ test.py
@@ -0,0 +1,77 @@
+# print("Hello")
+# print("Hello")
+# print("Hello")
+# print("Hello")
+# print("Hello")
+# print("Hello")
+# print("Hello")
+# print("Hello")
+# print("Hello")
+
+
+# class Msit:
+#     def main2():
+#         print("test")
+# Msit.main2()
+
+
+# a=30.23
+# b=20+3j
+# c="asdfghjk"
+# d=False
+# print(type(a),type(b))
+# print(type(c))
+# print(type(d))
+
+
+
+# a=int(input("Enter a number -> "))
+# b=int(input("Enter second number -> "))
+# for x in range(a,b+1):
+#     print(x,end="+")
+
+
+
+# a=int(input("Enter a number -> "))
+# b=int(input("Enter second number -> "))
+# c=int(input("Enter special number -> "))
+# for x in range(a,b,c):
+#     print(x,end=" ")
+
+
+
+# a=input("Enter a string value -> ")
+# print("Length of the string => ", len(a))
+# b= a.upper()
+# c=a.lower()
+# print(b,"\n",c)
+
+
+#STRING FORMATTING
+# class Msit:
+#     def main():
+#         a=input("Enter your name -> ") #0
+#         b=input("Enter your address -> ") #1
+#         c=input("Enter your last name -> ") #2 
+#         d=input("Enter your age -> ") #3
+#         final_str="My name is {1}, My address is {0}, My Last name is {3}, My age is {2}"
+#         print(final_str.format(a,b,c,d))
+# Msit.main()
+
+
+
+
+
+
+
+
+a="""Hello My name is Google bot.
+I am a 
+AI 
+robot. 
+I was born in MSIT."""
+print(a)
+
+
+
+
