ecd2289b22a120cbb116b281b68b3fcf5fab9428
Mainak Deb <75970138+mainak-wq@users.noreply.github.com>
2024-01-10 11:48:10 +0530

    Update databaseMYSQL.py

--- databaseMYSQL.py
+++ databaseMYSQL.py
@@ -26,3 +26,17 @@
 # mycursor = mydb.cursor()
 
 # mycursor.execute("CREATE DATABASE python")
+
+
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="yourusername",
+  password="yourpassword",
+  database="mydatabase"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))")
